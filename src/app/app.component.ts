746bc38cc387f8a1714495bfe59da961378e59eb
Mohammadreza Fouladi <m.fouladi@NBSAfzar.Local>
2019-06-16 08:35:48 +0430

    add appComponent title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  opened = false;
+export class AppComponent implements OnInit{
+
+  constructor(private titleService: Title){}
+
+  public setTitle(newTitle: string){
+    this.titleService.setTitle(newTitle);
+  }
+
+  ngOnInit(){
+    this.setTitle("Parmok");
+  }
 }
